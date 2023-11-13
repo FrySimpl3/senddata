64e9f612f0d57b9eca2c49915b81ece846b2b992
Fry <nghicute902@gmail.com>
2023-11-13 23:19:43 +0700

    Initial commit

---
+++ senddata/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace senddata
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "get image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(964, 529);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Send Data";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(174, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(64, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Fry";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(244, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(155, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "https://media.discordapp.net/attachments/1155173230282293358/1173657459441795172/" +
+    "523-5236058_nh-icon-hot-hnh-hd-png-download.png";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(405, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(571, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "https://discord.com/api/webhooks/1173654870088241163/pCAbtqhP8MARNTJPAmOzktwKEbLT" +
+    "kbnuFw0tIqnFzp59Sl4TLi5b1sVVcY7wC6iWxKmH";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(988, 582);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ senddata/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace senddata
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsImage())
+            {
+                System.Drawing.Image image = Clipboard.GetImage();
+                if (image != null)
+                {
+                    pictureBox1.Image = image;
+                }
+            }
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            string webhookUrl = textBox3.Text; // Thay YOUR_WEBHOOK_URL bằng URL của Webhook Discord của bạn
+
+            if (pictureBox1.Image != null)
+            {
+                string webhookName = textBox1.Text; // Tên Webhook được nhập bởi người dùng
+                string avatarUrl = textBox2.Text; // Đường dẫn đến biểu tượng (avatar) được nhập bởi người dùng
+
+                using (HttpClient client = new HttpClient())
+                {
+                    using (MultipartFormDataContent content = new MultipartFormDataContent())
+                    {
+                        // Lưu ảnh từ PictureBox vào một MemoryStream
+                        MemoryStream imageStream = new MemoryStream();
+                        pictureBox1.Image.Save(imageStream, System.Drawing.Imaging.ImageFormat.Png);
+                        ByteArrayContent imageContent = new ByteArrayContent(imageStream.ToArray());
+                        imageContent.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data")
+                        {
+                            Name = "file",
+                            FileName = "image.png",
+                        };
+                        content.Add(imageContent);
+
+                        // Tạo đối tượng JSON chứa tên và biểu tượng
+                        var webhookData = new
+                        {
+                            name = webhookName,
+                            avatar_url = avatarUrl
+                        };
+
+                        string payload = JsonConvert.SerializeObject(webhookData);
+
+                        StringContent jsonContent = new StringContent(payload);
+                        jsonContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                        content.Add(jsonContent, "payload_json");
+
+                        // Gửi POST request với hình ảnh đính kèm và JSON payload
+                        HttpResponseMessage response = await client.PostAsync(webhookUrl, content);
+
+                        // Xem xét kết quả
+                        if (response.IsSuccessStatusCode)
+                        {
+                            MessageBox.Show("Hình ảnh đã được gửi thành công.");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Có lỗi xảy ra khi gửi hình ảnh.");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
